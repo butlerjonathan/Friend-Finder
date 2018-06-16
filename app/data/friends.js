ee9f5b8039aff712916a0090aef32ff6497bdd35
butlerjonathan <36801326+butlerjonathan@users.noreply.github.com>
2018-06-16 18:14:27 -0400

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+var friendArray = [
+    {
+    "name":"Lady Mormont of Bear Island",
+    "photo":"http://i.imgur.com/RI27xcF.jpg",
+    "scores":[
+       5,
+       1
+     ]
+   },  {
+    "name":"Queen Kaleesi",
+    "photo":"http://images6.fanpop.com/image/photos/37200000/Daenerys-Targaryen-Season-4-daenerys-targaryen-37215658-2832-4256.jpg",
+    "scores":[
+       1,
+       5
+     ]
+   },  {
+    "name":"Sir Jorah",
+    "photo":"http://www.ew.com/sites/default/files/i/2015/05/10/got-iain-glen.jpg",
+    "scores":[
+       3,
+       3
+     ]
+   }
+  /* {
+    "name":"Lady Mormont of Bear Island",
+    "photo":"http://i.imgur.com/RI27xcF.jpg",
+    "scores":[
+       5,
+       1,
+       4,
+       4,
+       5,
+       1,
+       2,
+       5,
+       4,
+       1
+     ]
+   },
+   {
+    "name":"Lady Mormont of Bear Island",
+    "photo":"http://i.imgur.com/RI27xcF.jpg",
+    "scores":[
+       5,
+       1,
+       4,
+       4,
+       5,
+       1,
+       2,
+       5,
+       4,
+       1
+     ]
+   }*/
+  ];
+  
+  
+  // export the array. 
+  module.exports = friendArray;
